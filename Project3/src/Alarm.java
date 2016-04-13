44e4ac230a6fae049c27b1da30eaf5863dd35983
roneilPMH <roneil_PMH@hotmail.com>
2016-04-13 18:29:44 -0400

    Modify Alarm Class

--- Project3/src/Alarm.java
+++ Project3/src/Alarm.java
@@ -1,4 +1,21 @@
 
 public class Alarm {
-
+	private Boolean isOn = false;
+	private String message = "";
+	
+	public String turnOnAlarm () {
+		if (isOn == false) {
+			isOn = true;
+			message = "The Alarm is on!";
+		}
+		return message;
+	}
+	
+	public String turnOffAlarm () {
+		if (isOn == false) {
+			isOn = true;
+			message = "The Alarm is off!";
+		}
+		return message;
+	}
 }
